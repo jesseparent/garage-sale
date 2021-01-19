ddda54247262702f13a883470eb1ef199622c301
Jani Muhlestein <janimuhlestein@gmail.com>
2021-01-19 11:23:11 -0700

    updated models to make more sense

--- server/models/index.js
+++ server/models/index.js
@@ -2,6 +2,5 @@ const User = require('./User');
 const Product = require('./Product');
 const Category = require('./Category');
 const Order = require('./Order');
-const Specification = require('./Specification');
 
-module.exports = { User, Product, Category, Order, Specification };
+module.exports = { User, Product, Category, Order };
