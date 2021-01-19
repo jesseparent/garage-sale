d50fdb97b4d8ee1fa98e52478cbc2d550beb7780
Jani Muhlestein <janimuhlestein@gmail.com>
2021-01-18 18:24:10 -0700

    added review to models

---
+++ server/models/Review.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const reviewSchema = new Schema({
+  reviewer: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  reviewBody: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Category = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
--- server/models/index.js
+++ server/models/index.js
@@ -2,6 +2,6 @@ const User = require('./User');
 const Product = require('./Product');
 const Category = require('./Category');
 const Order = require('./Order');
-const Specifications = require('./Specification');
+const Specification = require('./Specification');
 
-module.exports = { User, Product, Category, Order, Specifications };
+module.exports = { User, Product, Category, Order, Specification };
