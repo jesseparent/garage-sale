a1f2b21c2aa9dd93e5b921bff2fe083679e24a54
cornwalljeremy <cornwall.jeremy@gmail.com>
2021-01-21 19:39:21 -0700

    Layout fun!

--- componets/itemCard/itemCard.js
+++ componets/itemCard/itemCard.js
@@ -21,6 +21,21 @@ function ItemCard(props) {
               <strong>Price</strong> {props.price}
             </li>
             <li>
+              <strong>Quantity</strong> {props.quantity}
+            </li>
+            <li>
+              <strong>Category</strong> {props.category}
+            </li>
+            <li>
+              <strong>Age</strong> {props.age}
+            </li>
+            <li>
+              <strong>Condition</strong> {props.condition}
+            </li>
+            <li>
+              <strong>Seller</strong> {props.seller}
+            </li>
+            <li>
               <button class="see-more">See More</button>
             </li>
           </ul>
