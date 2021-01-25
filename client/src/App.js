fadf68ade47e1ee346475fde5242c810b29321a9
cornwalljeremy <cornwall.jeremy@gmail.com>
2021-01-24 22:48:04 -0700

    Adding products

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,32 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
 import Home from "./pages/Home";
 import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Nav from "./components/Nav";
-import DetailBrief from './components/DetailBrief'
+import DetailBrief from "./components/DetailBrief";
 import { StoreProvider } from "./utils/GlobalState";
+import AddProduct from "./pages/AddProduct";
 
 // import ImageUpload from './pages/ImageUpload';
 // import GitImage from 'components/GetImage';
 
 // import ItemCard from './components/ItemCard';
-import Detail from './pages/Detail';
+import Detail from "./pages/Detail";
 
 const client = new ApolloClient({
   request: (operation) => {
-    const token = localStorage.getItem('id_token')
+    const token = localStorage.getItem("id_token");
     operation.setContext({
       headers: {
-        authorization: token ? `Bearer ${token}` : ''
-      }
-    })
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
   },
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 function App() {
@@ -39,6 +40,7 @@ function App() {
               <Route exact path="/" component={Home} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/signup" component={Signup} />
+              <Route exact path="/add" component={AddProduct} />
               <Route exact path="/product" component={Detail} />
               <Route exact path="/navCard" component={DetailBrief} />
               <Route component={NoMatch} />
@@ -47,7 +49,6 @@ function App() {
         </div>
       </Router>
     </ApolloProvider>
-
   );
 }
 
