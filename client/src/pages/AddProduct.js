33a83c34252d825d3470a5caba704b7eb100186a
Asher Clawson <31211822+KingPogona@users.noreply.github.com>
2021-01-25 19:47:31 -0700

    fix merge for AddProduct.js

--- client/src/pages/AddProduct.js
+++ client/src/pages/AddProduct.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { useMutation } from "@apollo/react-hooks";
 import Auth from "../utils/auth";
 
 import { useStoreContext } from "../utils/GlobalState";
 import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../utils/actions';
 import { idbPromise } from '../utils/helpers';
 
-// import { useMutation } from '@apollo/react-hooks';
+import { useMutation } from '@apollo/react-hooks';
 import { useQuery } from '@apollo/react-hooks';
 import { ADD_PRODUCT } from "../utils/mutations";
+import { QUERY_CATEGORIES } from "../utils/queries";
+
 import { Form, Button, Container } from "react-bootstrap";
+
 
 function AddProduct(props) {
   const [state, dispatch] = useStoreContext();
@@ -18,18 +20,45 @@ function AddProduct(props) {
   const { categories } = state;
 
   const [formState, setFormState] = useState({
-    category: "",
-    name: "",
-    description: "",
+    category: '',
+    name: '',
+    description: '',
     price: 0,
-    age: "",
-    condition: "",
-    model: "",
+    age: '',
+    condition: '',
+    model: '',
   });
 
   const [addProduct] = useMutation(ADD_PRODUCT);
 
-  const handleFormSubmit = async (event) => {
+  const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
+
+  useEffect(() => {
+
+    // console.log(data)
+    console.log(categoryData)
+
+
+
+    if (categoryData) {
+      dispatch({
+        type: UPDATE_CATEGORIES,
+        categories: categoryData.categories
+      });
+      categoryData.categories.forEach(category => {
+        idbPromise('categories', 'put', category);
+      });
+    } else if (!loading) {
+      idbPromise('categories', 'get').then(categories => {
+        dispatch({
+          type: UPDATE_CATEGORIES,
+          categories: categories
+        });
+      });
+    }
+  }, [categoryData, loading, dispatch]);
+
+  const handleFormSubmit = async event => {
     event.preventDefault();
     const mutationResponse = await addProduct({
       variables: {
@@ -40,23 +69,26 @@ function AddProduct(props) {
         age: formState.age,
         condition: formState.condition,
         model: formState.model,
-      },
+        // seller: context.user._id
+      }
     });
     if (mutationResponse) {
-      console.log("it worked! The returned data is the line below.");
+      console.log('it worked! The returned data is the line below.')
       console.log(mutationResponse);
       console.log(mutationResponse.data.addProduct._id)
 
       props.history.push('/imageupload/' + mutationResponse.data.addProduct._id);
     }
+
   };
 
-  const handleChange = (event) => {
+  const handleChange = event => {
     const { name, value } = event.target;
     setFormState({
       ...formState,
-      [name]: value,
+      [name]: value
     });
+    // console.log(formState);
   };
 
   return (
@@ -74,21 +106,29 @@ function AddProduct(props) {
               onChange={handleChange}
             />
           </Form.Group>
-          <Form.Group>
-            <Form.Label>Choose a category:</Form.Label>
-            <Form.Control
+
+            {/* load categories and populate the category dropdown */}
+      {state.categories.length ? (
+        <Form.Group>
+          <Form.Label>Choose a category:</Form.Label>
+          <Form.Control
               as="select"
               id="category"
               name="category"
               onChange={handleChange}
             >
-              <option value="600d09e0c006b06e3817a3e7">Tech</option>
-              <option value="600d09e9c006b06e3817a3e8">Art</option>
-              <option value="600d09f7c006b06e3817a3e9">clothing</option>
-              {/* <option>4</option> */}
-              {/* <option>5</option> */}
+              
+                {state.categories.map(category => (
+                  <option value={category._id}>{category.name}</option>
+                ))}
+            
             </Form.Control>
-          </Form.Group>
+            </Form.Group>
+      ) : (
+        <h3>You haven't added any products yet!</h3>
+      )}
+      { loading ? 
+      <h3>loading...</h3>: null}
 
           <Form.Group controlId="exampleForm.ControlTextarea1">
             <Form.Label>Description:</Form.Label>
@@ -105,7 +145,7 @@ function AddProduct(props) {
           <Form.Group>
             <Form.Label>Price: </Form.Label>
             <Form.Control
-              placeholder=""
+              placeholder="88"
               name="price"
               type="number"
               id="price"
@@ -144,22 +184,14 @@ function AddProduct(props) {
               onChange={handleChange}
             ></Form.Control>
           </Form.Group>
-          <Form.Group controlId="formBasicCheckbox">
-            <Form.Check
-              type="checkbox"
-              label="Would You Like to Add a Picture?"
-            />
-          </Form.Group>
           <Button variant="dark rounded-0" type="submit">
             Submit
-          </Button>
+           </Button>
         </Form>
       </Container>
-    </div>
-    /* <Link to="/login">
-        ← Go to Login
-      </Link> */
+    </div >
   );
+
 }
 
 export default AddProduct;
