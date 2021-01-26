ee623604f88f90d78621eb5db695fa2de7e844d5
Jesse Parent <jesseparent@gmail.com>
2021-01-25 17:11:21 -0700

    Merge conflict resolution for App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,17 +7,6 @@ import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Nav from "./components/Nav";
-<<<<<<< HEAD
-import DetailBrief from "./components/DetailBrief";
-import { StoreProvider } from "./utils/GlobalState";
-import AddProduct from "./pages/AddProduct";
-
-// import ImageUpload from './pages/ImageUpload';
-// import GitImage from 'components/GetImage';
-
-// import ItemCard from './components/ItemCard';
-import Detail from "./pages/Detail";
-=======
 import ChatApp from "./chat/components/ChatApp";
 import DetailBrief from './components/DetailBrief';
 import { StoreProvider } from "./utils/GlobalState";
@@ -25,7 +14,6 @@ import AddProduct from './pages/AddProduct';
 import Detail from './pages/Detail';
 import ImageUpload from './pages/ImageUpload';
 // import ItemCard from './componets/ItemCard';
->>>>>>> be3e116a38106c6213862a71c6058385084acb20
 
 const client = new ApolloClient({
   request: (operation) => {
@@ -51,10 +39,7 @@ function App() {
               <Route exact path="/" component={Home} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/signup" component={Signup} />
-<<<<<<< HEAD
-=======
               <Route exact path="/chat" component={ChatApp} />
->>>>>>> be3e116a38106c6213862a71c6058385084acb20
               <Route exact path="/add" component={AddProduct} />
               <Route exact path="/product" component={Detail} />
               <Route exact path="/navCard" component={DetailBrief} />
