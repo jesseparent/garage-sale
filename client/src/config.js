962cf28ed0fcd2e21a3151bdcb0e8aa4553fd63d
Asher Clawson <31211822+KingPogona@users.noreply.github.com>
2021-01-26 17:21:14 -0700

    convert cloudinary upload to use cloudinary-react and get rid of all old cloudinary related files

--- client/src/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = process.env.NODE_ENV === 'production'
-// change the heroku to match ours when created
-// if image upload stops working on heroku this might be the cause...
-  ? 'https://garage-sale-project.herokuapp.com'
-  : 'http://localhost:3001'
